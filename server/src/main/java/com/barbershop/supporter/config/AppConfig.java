7adbf44d774c02a3812012109d6c925bcb9cd802
MohammadReza <mohammad.shobiri@gmail.com>
2019-04-27 21:02:49 +0430

    init server

--- server/src/main/java/com/barbershop/supporter/config/AppConfig.java
+++ server/src/main/java/com/barbershop/supporter/config/AppConfig.java
@@ -1,13 +1,12 @@
-package ir.mr1213.restapi.config;
+package com.barbershop.supporter.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
-import org.springframework.stereotype.Component;
 
 
 @ComponentScans({
-        @ComponentScan("ir.mr1213.restapi.controller")})
+        @ComponentScan("com.barbershop.supporter.controller")})
 @SpringBootApplication
 public class  AppConfig{
 
