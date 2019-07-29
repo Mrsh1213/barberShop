9df18d664eafa77608276c1b72a951ca6b00f028
MohammadReza <mohammad.shobiri@gmail.com>
2019-07-29 09:42:00 +0430

    init server

--- client/src/pages/SignIn.js
+++ client/src/pages/SignIn.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import {withRouter} from "react-router-dom";
-import {Container,Card, Col, Row,Button} from "react-bootstrap";
+import {Container, Card, Col, Row, Button} from "react-bootstrap";
 import BSTextField from "../component/Primary/BSTextField";
 import {FaUser, FaLock} from "react-icons/lib/fa";
-
 
 
 function SignIn() {
@@ -16,25 +15,27 @@ function SignIn() {
 
     return (<Container>
             <Row className={"py-5"}>
-                <Col  md={3} sm={1} />
-                <Col  md={6} sm={10} >
-                    <Card border={"primary"} className={"py-5"}  body>
-                    <Row >
-                        <Col  md={12} sm={12}>
-                            <BSTextField value={username} setValue={setUsername} placeholder={"نام کاربری"} errors={[]}
-                                         label={<FaUser/>}/>
-                        </Col>
-                        <Col  md={12} sm={12}>
-                            <BSTextField value={password} setValue={setPassword} placeholder={"رمز عبور"} errors={[]}
-                                         label={<FaLock/>}/>
-                        </Col>
-                        <Col  md={12} sm={12}>
-                            <Button block variant="primary" size={"md"}>ورود</Button>
-                        </Col>
-                    </Row>
+                <Col md={3} sm={1}/>
+                <Col md={6} sm={10}>
+                    <Card border={"primary"} bg={"light"} className={"py-5"} body>
+                        <Row>
+                            <Col md={12} sm={12}>
+                                <BSTextField value={username} setValue={setUsername} placeholder={"نام کاربری"}
+                                             errors={[]}
+                                             label={<FaUser/>}/>
+                            </Col>
+                            <Col md={12} sm={12}>
+                                <BSTextField value={password} setValue={setPassword} placeholder={"رمز عبور"}
+                                             errors={[]}
+                                             label={<FaLock/>}/>
+                            </Col>
+                            <Col md={12} sm={12}>
+                                <Button block variant="primary" size={"md"}>ورود</Button>
+                            </Col>
+                        </Row>
                     </Card>
                 </Col>
-                <Col md={3}  sm={1}/>
+                <Col md={3} sm={1}/>
             </Row>
 
         </Container>
